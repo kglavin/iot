255a1d291c49b46152ac91bb62c8ecd50b6a91b5
kglavin <kglavin@gmail.com>
2018-09-09 09:51:46 -0700

    runnable on micropython and a second device id

--- micropython/network/job_machine.py
+++ micropython/network/job_machine.py
@@ -1,9 +1,11 @@
 import time
 import urequests as requests
 import json
+import machine
 
 class JobMachine:
     ttl = 0
+    uid = 'def'
     valid_op_codes = [ 'default']
 
 #    default_job_dict = {
@@ -63,35 +65,44 @@ class JobMachine:
                     return 'nok'
 
     def do_config(self,val={}):
-        print("     do_config:",val)
-        id = '70a1b200'
-        r = requests.get('http://192.168.2.1:2000/config/'+id)
-        if r.status_code == 200:
-            self.job_dict = json.loads(r.text)
-            self.valid_op_codes = self.job_dict['default']['valid_op_codes']
+        try:
+            m_id = machine.unique_id()
+            self.u_id = '{:02x}{:02x}{:02x}{:02x}'.format(m_id[0], m_id[1], m_id[2], m_id[3])
+            print("     do_config:",val)
+            r = requests.get('http://192.168.2.1:2000/config/'+self.u_id)
+            if r.status_code == 200:
+                self.job_dict = json.loads(r.text)
+                self.valid_op_codes = self.job_dict['default']['valid_op_codes']
+        except Exception as e:
+            print(e)
+            return 'nok'
+        
         return 'ok'
 
 class StatsMachine(JobMachine):
 
     def do_stat(self,val={}):
-        print("     do_stat",val)
-        u_id = '70a1b200'
-        gateway_url = 'http://192.168.2.1:2000/'
-        ds = dict()
-        ds['xvib'] = 3500 
-        ds['yvib'] = 27000
-        ds['hfac'] = 2.45
-        ds['rpm']  = 5600
-        ds['temp'] = 55
-        ds['ts'] = str((1,2,3))
-        ds['id'] = u_id
-        ds['sub'] = 42
-        ds['indx'] = 1
-        dd=dict()
-        dd['data'] = ds
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        r = requests.post(gateway_url+'data/'+u_id,data=json.dumps(dd), headers=headers)
-        print("post: %s %s " %( ds['indx'],r.status_code))
+        try:
+            print("     do_stat",val)
+            gateway_url = 'http://192.168.2.1:2000/'
+            ds = dict()
+            ds['xvib'] = 3500 
+            ds['yvib'] = 27000
+            ds['hfac'] = 2.45
+            ds['rpm']  = 5600
+            ds['temp'] = 55
+            ds['ts'] = str((1,2,3))
+            ds['id'] = self.u_id
+            ds['sub'] = 42
+            ds['indx'] = 1
+            dd=dict()
+            dd['data'] = ds
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            r = requests.post(gateway_url+'data/'+self.u_id,data=json.dumps(dd), headers=headers)
+            print("post: %s %s " %( ds['indx'],r.status_code))
+        except Exception as e:
+            print(e)
+        
         return 'ok'
 
     def do_sleep(self,val={}):
@@ -114,5 +125,5 @@ class StatsMachine(JobMachine):
 
 my_job = StatsMachine()
 my_job.do_config()
-my_job.run(num_loops=500)
+my_job.run(num_loops=50000)
 
